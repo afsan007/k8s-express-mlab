ac81b3d66522e92fcd058a9de9b9bef026643b9e
Afsan <af.hadafi@gmail.com>
2020-04-28 11:40:48 +0430

    change to k8s folder

--- k8s/make.sh
+++ k8s/make.sh
@@ -1,5 +1,11 @@
+echo "-->>> secrets"
 kubectl create secret generic mlab-credential \
---from-literal=MONGODB_USERNAME=afsan007 \
---from-literal=MONGODB_PASSWORD=af930611040
+--from-literal=MLAB_USERNAME=afsan007 \
+--from-literal=MLAB_PASSWORD=af930611040
+--from-literal=MLAB_URL=ds261648.mlab.com
 
+echo "-->>> apply configurations"
 kubectl apply -f .
+
+echo "-->>> monitoring"
+watch kubectl get all
