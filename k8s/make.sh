488a6d5c470a16e687978908261afd5ab1314126
Afsan <af.hadafi@gmail.com>
2020-04-29 15:54:12 +0430

    k8s

--- k8s/make.sh
+++ k8s/make.sh
@@ -1,8 +1,8 @@
 echo "-->>> secrets"
 kubectl create secret generic mlab-credential \
 --from-literal=MLAB_USERNAME=afsan007 \
---from-literal=MLAB_PASSWORD=af930611040
---from-literal=MLAB_URL=ds261648.mlab.com
+--from-literal=MLAB_PASSWORD=af930611040 \
+--from-literal=MLAB_URL=mlab
 
 echo "-->>> apply configurations"
 kubectl apply -f .
